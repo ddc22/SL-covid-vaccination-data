472179104cd3c050434e81535739b2648807c4e0
Dehan <dehandecroos@gmail.com>
2021-08-18 09:45:25 +0530

    Initial changes

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Covid Vaccination Data",
+  },
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
